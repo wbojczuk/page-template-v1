7e94b7f037928daa3610fe80167dc7f1ba0cd6be
William Bojczuk <williambojczuk@gmail.com>
2024-01-23 04:44:56 -0500

    added seo fix for blog posts

--- src/app/(pages)/blog/[slug]/page.tsx
+++ src/app/(pages)/blog/[slug]/page.tsx
@@ -5,10 +5,28 @@ import { getDocumentBySlug } from "outstatic/server";
 import { getDocumentSlugs } from "outstatic/server";
 
 export async function generateMetadata({params}: {params: {slug: string}}): Promise<Metadata>{
-    const poststuff = getDocumentBySlug("posts", params.slug, ["title", "description"])
+    const poststuff = getDocumentBySlug("posts", params.slug, ["title", 'coverImage', "description"])
     return {
-        title: `${poststuff?.title} | Rome Digital Web Design`,
-        description: poststuff?.description
+        title: `${poststuff?.title}`,
+        description: poststuff?.description,
+        openGraph: {
+            title: `${poststuff?.title}`,
+            description: poststuff?.description,
+            type: "website",
+            images: [
+              {
+                url: poststuff?.coverImage || '',
+                width: 1920,
+                height: 1080,
+              }
+            ]
+          },
+          twitter: {
+            card: 'summary_large_image',
+            title: `${poststuff?.title}`,
+            description: poststuff?.description,
+            images: [ poststuff?.coverImage || '',]
+          },
         }
 }
 
