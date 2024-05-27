4438276215914d40662cfc1f575375080afd5e51
William Bojczuk <williambojczuk@gmail.com>
2024-05-26 21:31:36 -0400

    updated

--- src/app/(mainsite)/components/misc/Navbar/NavMultiOption.tsx
+++ src/app/(mainsite)/components/misc/Navbar/NavMultiOption.tsx
@@ -1,7 +1,7 @@
 import styles from "./navbar.module.css"
 import Link from "next/link"
+// import { usePathname } from "next/navigation"
 import { useEffect, useRef, useState, useImperativeHandle, forwardRef, ReactNode } from "react"
-import { InlineIcon } from "@iconify/react"
 
 interface optionProps {
     ref: any,
@@ -10,14 +10,18 @@ interface optionProps {
     title: string
 }
 
- const NavMultiOption = forwardRef((props: optionProps, servicesRef)=>{
 
+
+ const NavMultiOption = forwardRef((props: optionProps, servicesRef)=>{
+  // const pathname = usePathname()
     const linkElems = props.links.map((data, i)=>{
         return (<Link key={i} href={data.url}><li>{data.title}</li></Link>)
     })
 
     const listRef: any = useRef()
-    const [isActive, setIsActive] = useState(false)
+
+    const active: any = useRef()
+    active.current = false
 
     useEffect(()=>{
       const isOnTouch = window.matchMedia("(max-width: 990px)").matches
@@ -28,31 +32,40 @@ interface optionProps {
         window.addEventListener("click", (evt)=>{
           //@ts-ignore
           if(evt.target != listRef.current){
-            setIsActive(false)
+            active.current = false
+            toggleActive()
           }
         })
       }
-    })
+    }, [])
+
+    // useEffect(()=>{
+    //   active.current = false
+    //   toggleActive()
+    // }, [pathname])
 
     function toggleActive(){
       
-      if(!isActive){
+      if(active.current){
         //@ts-ignore
         servicesRef!.current.classList.add(styles.dropdownActive)
       }else{
         //@ts-ignore
         servicesRef!.current.classList.remove(styles.dropdownActive)
       }
-      setIsActive((old)=>!old)
+      
     }
 
   return (
     //@ts-ignore
     <div ref={servicesRef} className={`${styles.dropdownLink} ${styles.navLink}`}>
-              <li ref={listRef} className={`nav-noclose ${styles.dropdownList}`} onClick={toggleActive}>
+              <li ref={listRef} className={`nav-noclose ${styles.dropdownList}`} onClick={()=>{
+                active.current = !active.current
+                toggleActive()
+              }}>
               {(props.icon !== undefined) ? props.icon : <span className={styles.noIcon}></span>}
                 <span style={{pointerEvents: "none"}}>{props.title}</span>
-                <InlineIcon style={{pointerEvents: "none"}} icon={"mdi:menu-down"} width="50px" height="50px" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m7 10l5 5l5-5z"></path></svg>
               </li>
 
               <ul>
