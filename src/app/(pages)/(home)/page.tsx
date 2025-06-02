a34bee00a0eabd6122409c712816b3323b0099e8
William Bojczuk <williambojczuk@gmail.com>
2025-06-02 09:44:17 -0400

    uopdted

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,13 +1,9 @@
-import PageTitle from "@/app/(mainsite)/components/misc/PageTitle/PageTitle";
-import Experience from "../../../../fordev/Misc/Experience/Experience";
-
 
 
 export default function Home() {
   return (
     <>
-      <PageTitle title="" subtitle="" />
-      <Experience />
+
     </>
   )
 }
